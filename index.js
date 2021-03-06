e0404b8c3db7b625a8639122096c81010cd5d4bc
Lalit Kumar <66167112+prajapatilalit@users.noreply.github.com>
2021-03-06 18:21:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ const PORT = process.env.PORT || 8000;
 
 app.use(express.static(path.join(__dirname, "capstoneFrontend", "build")));
 
-app.get("/", (req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "capstoneFrontend", "build", "index.html"));
 });
 
