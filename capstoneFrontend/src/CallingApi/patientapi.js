380aca53af562d977e3963fe20c3923f2dfe0dd4
Lalit Kumar <66167112+prajapatilalit@users.noreply.github.com>
2021-03-12 16:35:09 +0530

    Update patientapi.js
    upadete heroku api

--- capstoneFrontend/src/CallingApi/patientapi.js
+++ capstoneFrontend/src/CallingApi/patientapi.js
@@ -59,7 +59,7 @@ export const pat_dets = (pats) => {
   console.log(t.token);
   console.log(pats);
   console.log(JSON.stringify(pats));
-  return fetch(`http://localhost:8000/catalog/patients/create`, {
+  return fetch(`https://git.heroku.com/medicaldatashareapp.git/catalog/patients/create`, {
     method: "POST",
     headers: {
       Accept: "application/json",
@@ -73,7 +73,7 @@ export const pat_dets = (pats) => {
 };
 
 export const getPatDetails = (id) => {
-  return fetch(`http://localhost:8000/catalog/patients/${id}`, {
+  return fetch(`https://git.heroku.com/medicaldatashareapp.git/catalog/patients/${id}`, {
     method: "GET",
   })
     .then((response) => {
@@ -83,7 +83,7 @@ export const getPatDetails = (id) => {
 };
 
 export const updatePatDetails = (body, id) => {
-  return fetch(`http://localhost:8000/catalog/patients/${id}/update`, {
+  return fetch(`https://git.heroku.com/medicaldatashareapp.git/catalog/patients/${id}/update`, {
     method: "PUT",
     headers: {
       Accept: "application/json",
@@ -96,7 +96,7 @@ export const updatePatDetails = (body, id) => {
 };
 
 export const getAllPatientDetails = () => {
-  return fetch(`http://localhost:8000/catalog/patients`, {
+  return fetch(`https://git.heroku.com/medicaldatashareapp.git/catalog/patients`, {
     method: "GET",
   })
     .then((response) => {
@@ -107,7 +107,7 @@ export const getAllPatientDetails = () => {
 
 export const uploadPatientReport = (file) => {
   console.log(JSON.stringify(file));
-  return fetch("http://localhost:8000/catalog/doctors/uploadReport", {
+  return fetch("https://git.heroku.com/medicaldatashareapp.git/catalog/doctors/uploadReport", {
     method: "POST",
     headers: {
       Accept: "multipart/form-data",
@@ -136,7 +136,7 @@ export const uploadPatientReport = (file) => {
 
 export const addPrescription = (data) => {
   console.log(data);
-  return fetch("http://localhost:8000/catalog/patients/prescriptions/create", {
+  return fetch("https://git.heroku.com/medicaldatashareapp.git/catalog/patients/prescriptions/create", {
     method: "POST",
     headers: {
       Accept: "application/json",
@@ -151,7 +151,7 @@ export const addPrescription = (data) => {
 };
 
 export const getPres = (id) => {
-  return fetch(`http://localhost:8000/catalog/patient/${id}/pres`, {
+  return fetch(`https://git.heroku.com/medicaldatashareapp.git/catalog/patient/${id}/pres`, {
     method: "GET",
   })
     .then((response) => {
@@ -162,7 +162,7 @@ export const getPres = (id) => {
 
 export const addGraphValues = (data) => {
   console.log(data);
-  return fetch("http://localhost:8000/catalog/doctor/create/graph", {
+  return fetch("https://git.heroku.com/medicaldatashareapp.git/catalog/doctor/create/graph", {
     method: "POST",
     headers: {
       Accept: "application/json",
@@ -177,7 +177,7 @@ export const addGraphValues = (data) => {
 };
 
 export const getPatGraph = (id) => {
-  return fetch(`http://localhost:8000/catalog/patient/${id}/graphvalues`, {
+  return fetch(`https://git.heroku.com/medicaldatashareapp.git/catalog/patient/${id}/graphvalues`, {
     method: "GET",
   })
     .then((response) => {
@@ -187,7 +187,7 @@ export const getPatGraph = (id) => {
 };
 
 export const allHospitals = () => {
-  return fetch(`http://localhost:8000/api/getAllHospitals`, {
+  return fetch(`https://git.heroku.com/medicaldatashareapp.git/api/getAllHospitals`, {
     method: "GET",
   })
     .then((response) => {
@@ -198,7 +198,7 @@ export const allHospitals = () => {
 }
 
 export const nearHospitals = (lat,long,radius) => {
-  return fetch(`http://localhost:8000/api/getHospital/${lat}/${long}/${radius}`, {
+  return fetch(`https://git.heroku.com/medicaldatashareapp.git/api/getHospital/${lat}/${long}/${radius}`, {
     method: "GET",
   })
     .then((response) => {
